3849afe618dc4863b75c1404af74ff8b7343a972
Luan <luan.andryl@gmail.com>
2019-10-04 15:45:08 -0300

    add: card component

---
+++ src/components/card/Card.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Icon from '../../assets/icons/Icon'
+
+const Card = (props) => {
+    console.log(props)
+    const style = {
+        backgroundImage: 'url('+props.movie.image+')'
+    }
+
+    return (
+        <div className="task--card">
+            <div className="image-wrapper">
+              <div className="image" style={style} ></div>
+            </div>
+            <div className="card--content">
+              <h1>{props.movie.title}</h1>
+              <span>{props.movie.duration} - {props.movie.year}</span>
+              <div className="card--text">
+                <h4>Plot</h4>
+                <p>{props.movie.plot}</p>
+              </div>
+              <div className="card--actions">
+                <div className="star--action">
+                  <h4>Stars</h4>
+                  <Icon type="star" className="space" />
+                  <Icon type="star" className="space" />
+                  <Icon type="star" className="space" />
+                  <Icon type="star" className="space" />
+                  <Icon type="star" className="space" />
+                </div>
+                <button className="App__button">Add</button>
+              </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
