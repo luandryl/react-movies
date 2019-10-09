665b9899340ef69bc84e78e8a2a6b8ae963141f5
Luan <luan.andryl@gmail.com>
2019-10-09 02:18:21 -0300

    add: input func

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 
 class Input extends Component {
 
+    constructor (props) {
+        super(props)
+    }
+
+    handleInput = (e) => {
+        this.props.textString(e.target.value)
+    }
+
     render () {
         return (
             <div className="task-input--actions center">
-                <input className="App__input" type="text" placeholder="type some movie title" />  
+                <input onChange={this.handleInput} className="App__input" type="text" placeholder="type some movie title" />  
             </div>
         )
     }
