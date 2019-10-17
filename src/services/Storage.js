eddd8aa25e5e09ee473667cd22956ec693e93513
Luan <luan.andryl@gmail.com>
2019-10-17 14:24:31 -0300

    add: remove from fav

--- src/services/Storage.js
+++ src/services/Storage.js
@@ -17,7 +17,20 @@ const load = () => {
     }
 }
 
+const remove = (id) => {
+    if (localStorage.getItem('movies')) {
+        const movies = JSON.parse(localStorage.getItem('movies'))
+        const moviesUptated = movies.filter(m => {
+            return m !== id
+        })
+        localStorage.setItem('movies', JSON.stringify(moviesUptated))
+    } else {
+        return []
+    }
+}
+
 export const Storage = {
     save,
-    load
+    load,
+    remove
 }
